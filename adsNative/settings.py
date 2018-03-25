78841e0c422a68ade302a4f731f9829b1064fe04
Umakant B <umakant.b@2adpro.com>
2018-03-25 16:11:45 +0530

    dajngo config

--- adsNative/settings.py
+++ adsNative/settings.py
@@ -91,10 +91,8 @@ WSGI_APPLICATION = 'adsNative.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'adsnative',
-        'USERNAME':'umakant',
-        'PASSWORD':'welcome@123'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
